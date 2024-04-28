1c91172df479075cb3e51c7f6d8b33f3421caaec
Ganna Mohammad <mrsheaven777@gmail.com>
2024-04-28 18:54:29 +0300

    db connection

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 let lodash = require('lodash');
 const express = require('express');
+const mongoose = require('mongoose')
+const dotenv = require("dotenv")
 const app = express();
 app.use(express.json()); // middleware
+
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("db connection successfull"))
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.listen(process.env.PORT || 5000, () => {
+        console.log("backend server running")
+
+})
 
