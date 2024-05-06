ace9176657128d93e34d2e3b8453c523123ca024
Fatma AbdelHadi <fatmaabdelhadi2002@gmail.com>
2024-05-06 03:01:07 +0300

    Finished and tested project CRUD

--- controllers/project.js
+++ controllers/project.js
@@ -29,23 +29,6 @@ const getProject = async (req, res) => {
   if (project) res.json(project);
 };
 
-// const deleteProject = async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const project = await Project.findById(id);
-//     if (!project) {
-//       throw new Error("Project not found");
-//     }
-//     // Delete the project
-//     await project.remove();
-
-//     console.log("Project deleted successfully");
-//   } catch (error) {
-//     console.error("Error deleting project:", error.message);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// };
-
 const deleteProject = async (req, res) => {
   try {
     const projectId = req.params.id;
@@ -59,27 +42,6 @@ const deleteProject = async (req, res) => {
   }
 };
 
-
-// ana w ganna
-// const updateProject = async (req, res) => {
-//     try {
-//         const id = req.params.id;
-//         const { tasks } = req.body;
-
-//         const updatedProject = await Project.findByIdAndUpdate(id, { tasks }, { new: true }).populate('tasks');
-
-//         if (!updatedProject) {
-//             return res.status(404).json({ error: 'Project not found' });
-//         }
-
-//         res.json(updatedProject);
-//     } catch (error) {
-//         console.error('Error updating project:', error.message);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// };
-
-// // abdo's
 const updateProject = async (req, res) => {
   try {
     const project = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
@@ -89,30 +51,6 @@ const updateProject = async (req, res) => {
     res.status(500).send("Error updating project");
   }
 };
-
-// // Function to update a project by ID
-// const updateProject = async (req, res) => {
-//     const projectId = req.params.id; // Assuming the project ID is passed in the request parameters
-//     const updateFields = req.body; // Assuming the updated fields are passed in the request body
-
-//     try {
-//         // Find the project by ID
-//         const project = await Project.findById(projectId);
-
-//         if (!project) {
-//             return res.status(404).json({ error: 'Project not found' });
-//         }
-//         // Update project fields
-//         Object.assign(project, updateFields);
-//         // Save the updated project
-//         await project.save();
-
-//         return res.status(200).json({ message: 'Project updated successfully', project });
-//     } catch (err) {
-//         console.error('Error updating project:', err);
-//         return res.status(500).json({ error: 'Internal server error' });
-//     }
-// };
 
 module.exports = {
   createProject,
