41a841169b7b00a0bcd44ce5d024af74668ced78
Fatma AbdelHadi <fatmaabdelhadi2002@gmail.com>
2024-06-10 15:44:57 +0300

    Removed obsolete route

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,8 @@ const router = express.Router()
 const userController = require('../controllers/user')
 
 router.post('/signup', userController.registerUser)
-router.post("/login", userController.loginUser);
+router.post("/login", userController.loginUser)
 router.get('/find/:id', userController.getUser)
-router.get('/email', userController.getUser)
 router.get('/all', userController.getAllUsers)
 router.put('/update/:id', userController.updateUser)
 router.delete('/delete/:id', userController.deleteUser)
